3c26528085c5fec0b8eb3c7b62dff1ef4b05e7e5
makino <c0b2114181@edu.teu.ac.jp>
2022-10-04 14:50:43 +0900

    練習５

--- ex02/calc.py
+++ ex02/calc.py
@@ -1,15 +1,22 @@
+from ast import Num
 import tkinter as tk
 import tkinter.messagebox as tkm
 
 def button_click(event):
     btn =event.widget
-    txt =btn["text"]
-    tkm.showinfo(txt,f"[{txt}]ボタンが押されました")
+    num =btn["text"]
+    entry.insert(tk.END, num)
 
 root = tk.Tk()
-root.geometry("300x500")
+root.geometry("300x700")
 
-r, c = 0, 0
+r, c = 0, 0 # r: 行を表す変数／c：列を表す変数
+
+entry = tk.Entry(root, width=10, font=(", 40"), justify="right") # 練習4
+entry.grid(row=0, column=0, columnspan=3)
+
+
+r, c = 1, 0
 
 for i ,num in enumerate(range(9, -1, -1), 1):
 
@@ -24,6 +31,7 @@ for i ,num in enumerate(range(9, -1, -1), 1):
     if i%3 == 0:
         r += 1
         c = 0
+
     
 
 

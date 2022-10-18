eebd8e9d6529abddce2f706d681f20ca6be49c5e
makino <c0b2114181@edu.teu.ac.jp>
2022-10-18 14:22:16 +0900

    練習4

--- ex03/maze.py
+++ ex03/maze.py
@@ -1,6 +1,19 @@
 import tkinter as tk
 
-root = tk.Tk()
-root.title("迷えるこうかとん")
 
-root.mainloop()
+
+
+if __name__ == "__main__" :
+    root = tk.Tk()
+    root.title("迷えるこうかとん")
+
+    canb = tk.Canvas(root, width = 1500, height= 900, bg = "black")
+
+    tori = tk.PhotoImage(file="ex03/fig/5.png")
+    cx, cy = 300, 400
+    canb.create_image(cx, cy, image=tori, tag="tori")
+
+    key = ""
+
+    canb.pack()
+    root.mainloop()

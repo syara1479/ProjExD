0210b48c8e946c30f4b5017eaa356fcd7fbb28d6
makino <c0b2114181@edu.teu.ac.jp>
2022-10-25 14:27:02 +0900

    練習3

--- ex04/dodge_bomb.py
+++ ex04/dodge_bomb.py
@@ -7,12 +7,20 @@ def main():
     scrn_sfc = pg.display.set_mode((1600, 900))
     bg_sfc = pg.image.load("ex04/fig/pg_bg.jpg")
     bg_rct = bg_sfc.get_rect()
+    #練習3
+    tori_sfc = pg.image.load("ex04/fig/6.png")
+    tori_sfc = pg.transform.rotozoom(tori_sfc, 0, 2.0)
+    tori_rct = tori_sfc.get_rect()
+    tori_rct.center = 900, 400
+    
+    #練習2
     clock = pg.time.Clock()
     while True:
         scrn_sfc.blit(bg_sfc, bg_rct)
         for event in pg.event.get():
             if event.type == pg.QUIT:
                 return
+        scrn_sfc.blit(tori_sfc, tori_rct)#練習3
         pg.display.update()
         clock.tick(1000)
     

c544bb96d54b550be4f43222ac4e163b8483be19
makino <c0b2114181@edu.teu.ac.jp>
2022-10-25 15:41:58 +0900

    練習8

--- ex04/dodge_bomb.py
+++ ex04/dodge_bomb.py
@@ -1,8 +1,19 @@
 import pygame as pg
 import sys
+from random import randint
+
+def check_bound(obj_rct, scr_rct):
+    yoko, tate = +1, +1
+    if obj_rct.left < scr_rct.left or scr_rct.right < obj_rct.right: 
+        yoko = -1
+    if obj_rct.top < scr_rct.top or scr_rct.bottom < obj_rct.bottom: 
+        tate = -1
+    return yoko, tate
 
 
 def main():
+    check = 1
+    
     pg.display.set_caption("逃げろ！こうかとん")
     scrn_sfc = pg.display.set_mode((1600, 900))
     bg_sfc = pg.image.load("ex04/fig/pg_bg.jpg")
@@ -12,7 +23,15 @@ def main():
     tori_sfc = pg.transform.rotozoom(tori_sfc, 0, 2.0)
     tori_rct = tori_sfc.get_rect()
     tori_rct.center = 900, 400
-    
+    #練習5
+    bomb_sfc = pg.Surface((20,20)) #空のサーフェイス
+    bomb_sfc.set_colorkey((0, 0, 0))
+    pg.draw.circle(bomb_sfc, ( 255, 0, 0), (10,10), 10)#円を描く
+    bomb_rct = bomb_sfc.get_rect()
+    bomb_rct.centerx, bomb_rct.centery = randint(0, bg_rct.width), randint(0, bg_rct.height) 
+    #練習6
+    vx, vy = 1, 1
+
     #練習2
     clock = pg.time.Clock()
     while True:
@@ -21,18 +40,44 @@ def main():
             if event.type == pg.QUIT:
                 return
         #練習4
-        key_lst = pg.key.get_pressed()
-        if key_lst[pg.K_UP]:
-            tori_rct.centery -=1
-        if key_lst[pg.K_DOWN]:
-            tori_rct.centery +=1
-        if key_lst[pg.K_LEFT]:
-            tori_rct.centerx -=1
-        if key_lst[pg.K_RIGHT]:
-            tori_rct.centerx +=1
+        if check == 1:
+            key_lst = pg.key.get_pressed()
+            if key_lst[pg.K_UP]:
+                tori_rct.centery -=1
+            if key_lst[pg.K_DOWN]:
+                tori_rct.centery +=1
+            if key_lst[pg.K_LEFT]:
+                tori_rct.centerx -=1
+            if key_lst[pg.K_RIGHT]:
+                tori_rct.centerx +=1
+        if check == 2:
+            pg.mouse.get_pose()
+
+        yoko, tate = check_bound(tori_rct, bg_rct)
+        if yoko == -1:
+            if key_lst[pg.K_LEFT]: 
+                tori_rct.centerx += 1
+            if key_lst[pg.K_RIGHT]:
+                tori_rct.centerx -= 1
+        if tate == -1:
+            if key_lst[pg.K_UP]: 
+                tori_rct.centery += 1
+            if key_lst[pg.K_DOWN]:
+                tori_rct.centery -= 1       
         scrn_sfc.blit(tori_sfc, tori_rct)#練習3
+        yoko, tate = check_bound(bomb_rct, bg_rct)
+        vx *= yoko
+        vy *= tate
+        bomb_rct.move_ip(vx, vy)         #練習6
+        scrn_sfc.blit(bomb_sfc, bomb_rct)#練習5
+
+        # 練習8
+        if tori_rct.colliderect(bomb_rct): # こうかとんrctが爆弾rctと重なったら
+            return
+
         pg.display.update()
         clock.tick(1000)
+
     
 
 
